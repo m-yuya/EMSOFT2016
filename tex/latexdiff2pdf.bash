e715d7eb6b017e9670e174a9bb429e3be533fc12
m-yuya <cir9le@gmail.com>
2016-12-03 01:58:47 +0900

    Modify make diff

--- tex/latexdiff2pdf.bash
+++ tex/latexdiff2pdf.bash
@@ -6,7 +6,7 @@ printf "\nSelect *.tex: "
 read  old_file
 old_file=$old_file.tex
 echo $old_file
-latexdiff diff_tex/$old_file emsoft2016.tex > diff.tex
+latexdiff diff_tex/$old_file $1.tex > diff.tex
 platex diff.tex
 pbibtex diff
 platex diff.tex
